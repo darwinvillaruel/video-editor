db835f7e79defdd8b061039393110fa5c99ac992
Darwin <darwinvillaruel2215@gmail.com>
2024-09-05 13:54:50 +0800

    added video src

--- src/components/Expo.tsx
+++ src/components/Expo.tsx
@@ -5,6 +5,8 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { useState, useEffect } from "react";
 import { Data, data } from "./data/data";
 import Autoplay from "embla-carousel-autoplay";
+import { CldVideoPlayer } from "next-cloudinary";
+import "next-cloudinary/dist/cld-video-player.css";
 
 export default function Expo() {
   const [orientation, setOrientation] = useState<string>("horizontal");
@@ -37,25 +39,39 @@ export default function Expo() {
         }}
         plugins={[
           Autoplay({
-            delay: 5000,
+            delay: 6000,
             playOnInit: true,
             active: true,
           }),
         ]}>
-        <CarouselContent className="md:w-1/4">
+        <CarouselContent className="md:w-1/3">
           {data.map((item: Data, index: number) => (
             <CarouselItem key={index}>
-              <Card className="h-full">
-                <CardHeader>
-                  <CardTitle>{item.title}</CardTitle>
-                  <CardDescription>{item.description}</CardDescription>
-                </CardHeader>
-                <CardContent>VIDEO</CardContent>
+              <CldVideoPlayer
+                src={`videos/${item.source}`}
+                width="50"
+                height="50"
+                className="rounded-lg"
+              />
+              <Card className="flex flex-col h-full">
+                {/* <CardContent>
+                  <CardHeader className="px-0">
+                    <CardTitle>{item.title}</CardTitle>
+                    <CardDescription>{item.description}</CardDescription>
+                  </CardHeader>
+                  <CldVideoPlayer
+                    src={`videos/${item.source}`}
+                    width="500"
+                    height="899"
+                    className="rounded-lg"
+                  />
+                </CardContent> */}
               </Card>
             </CarouselItem>
           ))}
         </CarouselContent>
       </Carousel>
+      <div>OK</div>
     </section>
   );
 }
--- src/components/data/data.tsx
+++ src/components/data/data.tsx
@@ -1,13 +1,7 @@
 interface Data {
   title: string;
   description: string;
-  source: Source;
-}
-
-interface Source {
-  src: string;
-  frameorder: number;
-  allow: string;
+  source: string;
 }
 
 export const data: Data[] = [
@@ -15,100 +9,60 @@ export const data: Data[] = [
     title: "Fika",
     description:
       "Created an engaging original video edit for “Fika”, a coffee shop located in Iloilo City. Used manual slow transitions and static close ups, used a slow song and matched it with the clips and added filters to have a slow vintage vibe.",
-    source: {
-      src: "https://player.vimeo.com/video/934266416?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
+    source: "fika",
   },
   {
     title: "Camiña Balay Nga Bato",
     description:
       "Created an engaging original video edit for “Camiña Balay Nga Bato”, a century-old heritage house in Iloilo City. Used a lot of static video, used a slow song and matched it with the clips and added filters to have a slow vintage vibe.",
-    source: {
-      src: "https://player.vimeo.com/video/935330669?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
+    source: "balay_bato",
   },
   {
     title: "Messy Donuts",
     description:
       "Created an engaging original video edit for “Messy Donuts”, a donuts and and coffee business located in Iloilo City. Used a few transitions of the editing software and a lot of close ups to highlight the food and the process of making it.",
-    source: {
-      src: "https://player.vimeo.com/video/934415783?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
+    source: "messy_donuts",
   },
   {
     title: "Sweet or Savory",
     description:
       "Created an engaging original video edit for “Sweet or Savory”, a homemade ice cream and food shop business located in Iloilo City. Used a lot of close ups and static video, used a slow song and matched it with the clips and added filters to have a slow vintage vibe.",
-    source: {
-      src: "https://player.vimeo.com/video/934415732?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
-  },
-  {
-    title: "Carl and Razel",
-    description:
-      "Created an engaging original video edit for Carl and Razel Cenal for their wedding. Used a simpler editing, with matching dreamy filter and matched the clips with the song to have a clean lovely wedding video.",
-    source: {
-      src: "https://player.vimeo.com/video/935331085?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
-  },
-  {
-    title: "Wedding Transition",
-    description:
-      "Created a wedding transition video with friends. Used a lot of manual transitions and used a hype song for the climax of the video.",
-    source: {
-      src: "https://player.vimeo.com/video/934415750?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
-  },
-  {
-    title: "Road Trip",
-    description:
-      "Created a casual video with friends about night drives and eating out. Used some manual transitions and used a song with a bit of hype to match the vid's racy theme.",
-    source: {
-      src: "https://player.vimeo.com/video/934415764?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
-  },
-  {
-    title: "Chili's",
-    description:
-      "Created an engaging original video edit for “Chili's” for a gig, a famous American casual dining restaurant chain. Used manual transitions and static close ups, used a fast paces song and matched it with the clips.",
-    source: {
-      src: "https://player.vimeo.com/video/934415764?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
-  },
-  {
-    title: "Convenience Coffeehouse",
-    description:
-      "Created an original video edit for “Convenience Coffeehouse”, a slow bar, coffee shop hidden in Makati City. Used a lot of overlays and sound effects for each cut and color graded it to give it a film vibe.",
-    source: {
-      src: "https://player.vimeo.com/video/934415764?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
+    source: "sweet_savory",
   },
   {
     title: "Kalye",
     description:
       "Created an engaging original video edit for “Kalye”, a coffee shop located in Iloilo City. Used manual slow transitions and static close ups, used a vintage song and matched it with the cuts of the clips.",
-    source: {
-      src: "https://player.vimeo.com/video/934415764?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&portrait=0&byline=0",
-      frameorder: 0,
-      allow: "autoplay; fullscreen; picture-in-picture; clipboard-write",
-    },
+    source: "kalye",
+  },
+  {
+    title: "Chili's",
+    description:
+      "Created an engaging original video edit for “Chili's” for a gig, a famous American casual dining restaurant chain. Used manual transitions and static close ups, used a fast paces song and matched it with the clips.",
+    source: "chilis",
+  },
+  {
+    title: "Convenience Coffeehouse",
+    description:
+      "Created an original video edit for “Convenience Coffeehouse”, a slow bar, coffee shop hidden in Makati City. Used a lot of overlays and sound effects for each cut and color graded it to give it a film vibe.",
+    source: "coffeehouse",
+  },
+  {
+    title: "Carl and Razel",
+    description:
+      "Created an engaging original video edit for Carl and Razel Cenal for their wedding. Used a simpler editing, with matching dreamy filter and matched the clips with the song to have a clean lovely wedding video.",
+    source: "carl_razel",
+  },
+  {
+    title: "Wedding Transition",
+    description:
+      "Created a wedding transition video with friends. Used a lot of manual transitions and used a hype song for the climax of the video.",
+    source: "wedding",
+  },
+  {
+    title: "Road Trip",
+    description:
+      "Created a casual video with friends about night drives and eating out. Used some manual transitions and used a song with a bit of hype to match the vid's racy theme.",
+    source: "road_trip",
   },
 ];
